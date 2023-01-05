d9c02044c0e3a12609bfe1aa220b7be7ea09fd5b
Steva <stevaalex@github.com>
2023-01-05 19:09:09 +0000

    Added psuedocode

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//load code when js doc is fully ready
+$(document).ready(function () {  
+    ///variable that corrects value for input1=food ingredient 
+    //variable that collect value for input2=drink ingredient 
+    //variable that selects button to click to submit 
+
+    //when submit button is clicked input 1 is fed into API for food and input 2 is fed into API for drinks 
+
+    //once submit is clicked, the input sections, button, recent searched and suggested serached are hidden is hidden 
+    //as above, data attribute of the above changes to hidden?
+    //then data attributes for cards with suggested menu and cocktail changes to visible
+});
